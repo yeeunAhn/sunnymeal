0601831cac41c7603791e9cb358ca986dc0e2f33
hurler <yeeun780@gmail.com>
2025-04-06 13:34:07 +0900

    관리자페이지 수정필요 모바일날짜이슈

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./LoginPage";
 import { PointsPage } from "./PointsPage";
 import { SignupPage } from "./SignupPage";
 import AdminPage from "./AdminPage"; // default import 방식
-import FindAccountPage from ".FindAccountPage";
+import FindAccountPage from "./FindAccountPage";
 
 function App() {
   return (
