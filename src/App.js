d996d99003bbde5c839a00f3058cb1c46a20e4fa
hurler <yeeun780@gmail.com>
2025-04-24 22:22:10 +0900

    왜안돼

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./LoginPage";
-import PointsPage from "./PointsPage";
-import SignupPage from "./SignupPage";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./LoginPage";
+import { PointsPage } from "./PointsPage";
+import { SignupPage } from "./SignupPage";
 import AdminPage from "./AdminPage";
 import FindAccountPage from "./FindAccountPage";
 import MemberPage from "./MemberPage";
