3b77580e115be980455746ab2ba4e32c431abb2e
hurler <yeeun780@gmail.com>
2025-04-24 22:23:44 +0900

    왜안돼

--- src/PointsPage.js
+++ src/PointsPage.js
@@ -4,7 +4,7 @@ import { db } from "./firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import "./PointsPage.css";
 
-function PointsPage() {
+export function PointsPage() {
   const { phone } = useParams(); // URL에서 phone 파라미터 추출
   const [remainingPoints, setRemainingPoints] = useState(null);
   const [orderHistory, setOrderHistory] = useState([]);
